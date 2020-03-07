24c8b65f2329c11c33d47f4d92333988ffac06e5
Alexander Skrock <alexanderskrock@gmx.de>
2020-03-07 16:11:02 +0100

    Added solution for sum-of-multiples

--- sum-of-multiples/SumOfMultiples.fs
+++ sum-of-multiples/SumOfMultiples.fs
@@ -1,3 +1,12 @@
 ﻿module SumOfMultiples
 
-let sum (numbers: int list) (upperBound: int): int = failwith "You need to implement this function."
+let sum (numbers: int list) (upperBound: int): int =
+    let isDivisable number divisor = number % divisor = 0
+    let isDivisableByAnyOf number =
+        numbers
+            |> List.filter ((<) 0)
+            |> List.exists (isDivisable number)
+    
+    [1 .. upperBound - 1]
+        |> List.filter isDivisableByAnyOf
+        |> List.sum
