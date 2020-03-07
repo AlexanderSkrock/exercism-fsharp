a0b0e749b8bf933f1c2a253c8028d09ecabe7ab4
Alexander Skrock <alexanderskrock@gmx.de>
2020-03-07 16:15:20 +0100

    Added solution for leap

--- leap/Leap.fs
+++ leap/Leap.fs
@@ -1,3 +1,5 @@
 ﻿module Leap
 
-let leapYear (year: int): bool = failwith "You need to implement this function."
+let leapYear (year: int): bool = 
+    let isDivisableBy number divisor = number % divisor = 0
+    isDivisableBy year 4 && (not (isDivisableBy year 100) || isDivisableBy year 400)
